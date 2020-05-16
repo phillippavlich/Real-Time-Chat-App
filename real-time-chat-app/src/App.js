aa6d131d4c598c55f9a0b30f136cdcd5499261dc
Phillip Pavlich <pavlicpm@mcmaster.ca>
2020-05-16 19:38:50 -0400

    Add header and future navbar

--- real-time-chat-app/src/App.js
+++ real-time-chat-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 
+import Nav from './components/Nav.js'
 import Users from './components/Users.js'
 import MessageLog from './components/MessageLog.js'
 import SendMessage from './components/SendMessage.js'
@@ -8,13 +9,7 @@ import SendMessage from './components/SendMessage.js'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Add content here
-        </p>
-
-      </header>
-
+      <Nav />
       <Users />
       <MessageLog />
       <SendMessage />
---
+++ real-time-chat-app/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+
+	render (){
+		return(
+			<div id = "navBar">
+				<h1>Welcome to my chat app! </h1>
+
+			</div>
+			);
+	}
+
+}
+
+export default Nav;
