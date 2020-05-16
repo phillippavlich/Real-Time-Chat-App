02997ffe99fc2314bcc4f335b72fc9fed7d1062f
Phillip Pavlich <pavlicpm@mcmaster.ca>
2020-05-16 19:20:44 -0400

    Add Users component with basic structure

--- real-time-chat-app/src/App.js
+++ real-time-chat-app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import Users from './components/Users.js'
+
 function App() {
   return (
     <div className="App">
@@ -8,8 +10,10 @@ function App() {
         <p>
           Add content here
         </p>
-        
+
       </header>
+
+      <Users />
     </div>
   );
 }
