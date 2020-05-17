eb72b620185cde46f899716e98dedd7943c22611
Phillip Pavlich <pavlicpm@mcmaster.ca>
2020-05-16 22:20:28 -0400

    dynamically populate online users, but using dummy data for now

--- real-time-chat-app/src/components/Users.js
+++ real-time-chat-app/src/components/Users.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 
 class Users extends Component {
+    constructor(){
+          super();
+          this.users=["Bob","Phil","Chad", "Marcus"];
+        }
 
 	render (){
 		return(
 			<div id = "usersBox">
-				<h3> <u>Online Users: </u></h3>
-				<ul>
-				    <li>test1</li>
-				    <li>phil</li>
-				    <li>test2</li>
+				<h3> <u>Online Users ({this.users.length}) : </u></h3>
+				<ul className="no-dots">
+				    {this.users.map((value, index) => {
+                        return <li key={index}>{value}</li>
+                      })}
 				</ul>
 			</div>
 			);
