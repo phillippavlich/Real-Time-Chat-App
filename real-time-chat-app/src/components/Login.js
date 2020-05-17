2e2ca842a801e8f35d43e65cbb858a93b512a2ec
Phillip Pavlich <pavlicpm@mcmaster.ca>
2020-05-17 00:07:38 -0400

    Save state of username from the login form and ensure user entered a username

--- real-time-chat-app/src/components/Login.js
+++ real-time-chat-app/src/components/Login.js
@@ -1,6 +1,26 @@
 import React, {Component} from 'react';
 
 class Login extends Component {
+    state={
+        user: ""
+    }
+
+    onChange =(e)=>{
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    onSubmit=(e)=>{
+        //prevent default submission
+        if(this.state.user === ""){
+            e.preventDefault();
+            alert("Sorry, you have not entered a username. Please try again.");
+        }
+        else{
+            alert("Welcome to the app " + this.state.user + "!");
+        }
+    }
 
 	render (){
 		return(
@@ -9,8 +29,8 @@ class Login extends Component {
 			    <h3>Made by: Phillip Pavlich</h3>
 			    <h4>A real time chat application using Socket.io to connect to other web users</h4>
                 <br />
-				<form action="/app">
-				  <input type="text" placeholder="Enter your username" />
+				<form action="/app" onSubmit={this.onSubmit}>
+				  <input type="text" name="user" placeholder="Enter your username" onChange={this.onChange} />
                   <input type="submit" value="Login" />
                 </form>
 			</div>
