3eec4574e8badddd19552eedb839509271bc84a3
Phillip Pavlich <pavlicpm@mcmaster.ca>
2020-05-16 22:05:26 -0400

    dynamically populate comments in react, using dummy data in constructor for now

--- real-time-chat-app/src/components/MessageLog.js
+++ real-time-chat-app/src/components/MessageLog.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
 
 class MessageLog extends Component {
+    constructor(){
+          super();
+          this.comments=[
+              {
+                  sender: "Phil",
+                  msg: "testing messages"
+              },
+              {
+                  sender: "Bob",
+                  msg: "i think i got it right this time"
+              },
+               {
+                   sender: "Chad",
+                   msg: "this is to test a very long message. I want to make sure that all of it is properly displayed and that the lines are wrapped"
+               }];
+       }
 
 	render (){
 		return(
 			<div id = "messageLogBox">
-			    <div className="comment"><strong>Phil:</strong> testing</div>
-				<div className="comment"><strong>Bob:</strong> testing2</div>
+			    {this.comments.map((value, index) => {
+			        return <div className="comment" key={index}> <strong> {value.sender}: </strong> {value.msg}</div>
+                  })}
 
 			</div>
 			);
