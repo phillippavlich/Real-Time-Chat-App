43a62a10b7cb7e2358cdb7a864162c0971911b7d
Phillip Pavlich <pavlicpm@mcmaster.ca>
2020-05-17 01:03:16 -0400

    Add state for comments and allow new comments to be added

--- real-time-chat-app/src/components/MessageLog.js
+++ real-time-chat-app/src/components/MessageLog.js
@@ -3,25 +3,36 @@ import React, {Component} from 'react';
 class MessageLog extends Component {
     constructor(){
       super();
-      this.comments=[
-          {
-              sender: "Phil",
-              msg: "testing messages"
-          },
-          {
-              sender: "Bob",
-              msg: "i think i got it right this time"
-          },
-           {
-               sender: "Chad",
-               msg: "this is to test a very long message. I want to make sure that all of it is properly displayed and that the lines are wrapped"
-           }];
+      this.addNewComment("Marcus", "hi");
+    }
+
+    state={
+        comments: [{
+                 sender: "Phil",
+                 msg: "testing messages"
+             },
+             {
+                 sender: "Bob",
+                 msg: "i think i got it right this time"
+             },
+              {
+                  sender: "Chad",
+                  msg: "this is to test a very long message. I want to make sure that all of it is properly displayed and that the lines are wrapped and correct"
+              }]
+    }
+
+    addNewComment=(user, comment)=>{
+        if(comment !== ""){
+            this.setState({
+                comments: this.state.comments.push({sender: user, msg: comment})
+            });
+        }
     }
 
 	render (){
 		return(
 			<div id = "messageLogBox">
-			    {this.comments.map((value, index) => {
+			    {this.state.comments.map((value, index) => {
 			        return <div className="comment" key={index}> <strong> {value.sender}: </strong> {value.msg}</div>
                   })}
 
