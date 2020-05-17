b62fa845f52757afd5239d76376e67ae6155c465
Phillip Pavlich <pavlicpm@mcmaster.ca>
2020-05-17 00:12:42 -0400

    fix title

--- real-time-chat-app/src/components/Login.js
+++ real-time-chat-app/src/components/Login.js
@@ -25,7 +25,7 @@ class Login extends Component {
 	render (){
 		return(
 			<div id = "loginForm">
-			    <h1>Real-Time-Chat-App</h1>
+			    <h1>Real Time Chat App</h1>
 			    <h3>Made by: Phillip Pavlich</h3>
 			    <h4>A real time chat application using Socket.io to connect to other web users</h4>
                 <br />
