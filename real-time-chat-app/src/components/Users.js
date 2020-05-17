ee8ebc06d39e38f35a5d74da627e5c523763d90e
Phillip Pavlich <pavlicpm@mcmaster.ca>
2020-05-17 00:24:18 -0400

    Save state of current users and add method to append new user

--- real-time-chat-app/src/components/Users.js
+++ real-time-chat-app/src/components/Users.js
@@ -3,15 +3,25 @@ import React, {Component} from 'react';
 class Users extends Component {
     constructor(){
           super();
-          this.users=["Bob","Phil","Chad", "Marcus"];
+          this.addNewUser("Carlos");
         }
+
+    state={
+        users: ["Bob","Phil","Chad", "Marcus", "Fred"]
+    }
+
+    addNewUser=(newName)=>{
+        this.setState({
+            users: this.state.users.push(newName)
+        });
+    }
 
 	render (){
 		return(
 			<div id = "usersBox">
-				<h3> <u>Online Users ({this.users.length}) : </u></h3>
+				<h3> <u>Online Users ({this.state.users.length}) : </u></h3>
 				<ul className="no-dots">
-				    {this.users.map((value, index) => {
+				    {this.state.users.map((value, index) => {
                         return <li key={index}>{value}</li>
                       })}
 				</ul>
